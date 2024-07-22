809c907815644061b1f4a023d9c59fd3a07a8cc0
shlokabari01 <119592364+shlokabari01@users.noreply.github.com>
2024-07-22 14:27:44 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,1 +1,25 @@
+def add(numbers: str) -> int:
+    if numbers.startswith('//'):
+        delimiter, numbers = parse_custom_delimiter(numbers)
+    else:
+        delimiter = ','
+    
+    num_list = split_numbers(numbers, delimiter)
+    return sum_valid_numbers(num_list)
+
+
+def parse_custom_delimiter(numbers: str) -> tuple:
+    delimiter, numbers = numbers[2:].split('\n', 1)
+    return delimiter, numbers
+
+
+def split_numbers(numbers: str, delimiter: str) -> list:
+    return numbers.replace('\n', delimiter).split(delimiter)
+
+
+def sum_valid_numbers(num_list: list) -> int:
+    def is_valid(num):
+        return num.isdigit() and int(num) <= 1000
+    
+    return sum(int(num) for num in num_list if is_valid(num))
 
