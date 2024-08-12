d5cf429e2440f0e67369a22cc0c63b4856efedcf
shlokabari01 <119592364+shlokabari01@users.noreply.github.com>
2024-08-12 12:25:55 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,17 +1,64 @@
-Upper_limit = 1000 
+#global variable
+Upper_limit = 1000;
 
-def add_cnn2(numbers):  
-    if not numbers:  
-        return 0  
+def add(numbers):
+
+    # If the input string is empty, return 0
+    if not numbers:
+        return 0
+    #default delimiter
+    delimiter = ';'
+
+    # Check the starting delimiter
+    if numbers.startswith('//'):
+        delimiter, numbers = Eliminating_Delimiter(numbers)
+
+    # Separating the numbers
+    List_of_Numbers = Separate_Numbers(numbers, delimiter)
     
-    delimiter = ';' 
-    if numbers.startswith('//'):  
-        delimiter, numbers = numbers[2:].split('\n', 1) 
+    #Check for upper limit of numbers
+    List_of_Numbers = Check_Numbers_Upper_Limit(List_of_Numbers)
 
-    numbers_list = [int(num) for num in numbers.replace('\n', delimiter).split(delimiter) if int(num) <= Upper_limit]  
+    # Check for negative numbers
+    Check_Negative_Numbers(List_of_Numbers)
 
-    negative_numbers = [num for num in numbers_list if num < 0]  
-    if negative_numbers:  
-        raise ValueError(f"Negatives not allowed: {', '.join(map(str, negative_numbers))}")  
+    # Calculate and return the sum of the numbers
+    Sum_of_numbersList = sum(List_of_Numbers)
     
-    return sum(numbers_list) 
+    return Sum_of_numbersList
+
+
+def Eliminating_Delimiter(numbers):
+
+    # Extract the delimiter and remove it from the numbers string
+    delimiter, numbers = numbers.split('\n', 1)
+    delimiter = delimiter[2:]  # Ignore the initial '//' characters
+    return delimiter, numbers
+
+
+def Separate_Numbers(numbers, delimiter):
+
+    # Replace newline characters with the delimiter and split the string
+    numbers = numbers.replace('\n', delimiter)
+    Numbers_separated = list(map(int, numbers.split(delimiter)))
+    
+    return Numbers_separated
+
+
+def Check_Negative_Numbers(numbers_list):
+
+    # Check for negative numbers
+    negative_numbers = [num for num in numbers_list if num < 0]
+    ErrorMessage(negative_numbers)
+    return negative_numbers
+
+def Check_Numbers_Upper_Limit (numbers_list):
+    
+    # Check for the upper limit of numbers
+    upper_limit_of_numbers = [num for num in numbers_list if num <= Upper_limit]
+    return upper_limit_of_numbers 
+
+def ErrorMessage(negative_numbers):
+    #Error message for negative numbers
+    if negative_numbers:
+        raise ValueError(f"Negatives not allowed: {', '.join(map(str, negative_numbers))}")
